966e400318a83f203dc3ca174f32eeeff706ec41
Matt McAuley <mattwilliam618@gmail.com>
2024-11-19 21:10:17 -0500

    Add files

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className={"flex-auto"}>
+      <Text className={'color-amber-600'}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
