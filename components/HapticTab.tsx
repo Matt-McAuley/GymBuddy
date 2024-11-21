e05117e420c6a429fdf772398c9b4c681b5144e1
Matthew McAuley <mattwilliam618@gmail.com>
2024-11-21 12:34:18 -0500

    Fix Tab button

--- components/HapticTab.tsx
+++ components/HapticTab.tsx
@@ -5,6 +5,7 @@ import * as Haptics from 'expo-haptics';
 export function HapticTab(props: BottomTabBarButtonProps) {
   return (
     <PlatformPressable
+        {...props}
       onPressIn={(ev) => {
         if (process.env.EXPO_OS === 'ios') {
           Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
