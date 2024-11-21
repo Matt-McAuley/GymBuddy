f178eb9fa692305587293d1ed46c4909a36de78f
Matt McAuley <mattwilliam618@gmail.com>
2024-11-21 00:26:28 -0500

    Add Haptic tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,25 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Image } from 'react-native';
+
+import { HapticTab } from '@/components/HapticTab';
+
+export default function TabLayout() {
+
+  return (
+    <Tabs
+      screenOptions={{
+      headerShown: false,
+      tabBarButton: HapticTab,
+      tabBarLabelStyle: {
+        color: 'black',
+      },
+      }}>
+      <Tabs.Screen name="index" options={{
+          title: 'Home',
+          tabBarIcon: () => <Image className={"h-10 w-10"} source={require("@/assets/images/HomeIcon.png")} />,
+        }}
+      />
+    </Tabs>
+  );
+}
