a705f1cc26e293ca3a7407684e33b4ca74f1416b
Matt McAuley <mattwilliam618@gmail.com>
2024-11-27 01:17:48 -0500

    Add recognition of swiping gesture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,11 +7,13 @@ import {addMockProgram, dbSetup, dbTeardown, getProgram} from "@/db/dbFunctions"
 import * as SQLite from 'expo-sqlite';
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 import {useStore} from "@/store";
+import {GestureDetector, Gesture, GestureHandlerRootView, Directions} from "react-native-gesture-handler";
+import {runOnJS} from "react-native-reanimated";
 
 export default function Index() {
     const [day, setDay] = useState("");
     const [color, setColor] = useState("");
-    const {program, setProgram} = useStore();
+    const {program, setProgram, currentDay, setCurrentDay} = useStore();
     const db = SQLite.openDatabaseSync('programs.db');
     useDrizzleStudio(db);
 
@@ -29,6 +31,20 @@ export default function Index() {
         setDay(firstDay!.name);
     }, [program]);
 
+    const panRight = Gesture.Fling().onEnd(() => {
+        console.log('panRight');
+        runOnJS(setCurrentDay)(Math.min(program!.days.length-1, Math.min(program!.days.length-1, currentDay + 1)));
+        runOnJS(setDay)(program!.days[currentDay].name);
+        runOnJS(setColor)(program!.days[currentDay].color);
+    }).direction(Directions.LEFT);
+
+    const panLeft = Gesture.Fling().onEnd(() => {
+        console.log('panLeft')
+        runOnJS(setCurrentDay)(Math.min(program!.days.length-1, Math.max(0, currentDay - 1)));
+        runOnJS(setDay)(program!.days[currentDay].name);
+        runOnJS(setColor)(program!.days[currentDay].color);
+    }).direction(Directions.RIGHT);
+
 
   return (program == null) ? (
       <View className={'flex justify-center items-center h-full'}>
@@ -36,11 +52,17 @@ export default function Index() {
       </View>
       )
       : (
-    <View className={'flex-1 flex-col justify-start items-center p-3 gap-4'}>
-        <Text className={`text-7xl font-bold color-${color}-500`}>{day}</Text>
-        <Timer/>
-        <Counter/>
-        <ExerciseDisplay/>
-    </View>
+          <GestureHandlerRootView>
+              <GestureDetector gesture={panLeft}>
+              <GestureDetector gesture={panRight}>
+                <View className={'flex-1 flex-col justify-start items-center p-3 gap-4'}>
+                    <Text className={`text-7xl font-bold color-${color}-500`}>{day}</Text>
+                    <Timer/>
+                    <Counter/>
+                    <ExerciseDisplay/>
+                </View>
+              </GestureDetector>
+              </GestureDetector>
+          </GestureHandlerRootView>
   );
 }
