141a6d79e054d8c4b0db5682bbc8315f55f44c8c
Matt McAuley <mattwilliam618@gmail.com>
2024-12-05 17:15:26 -0500

    Delete ios directory

--- components/Music/Login.tsx
+++ components/Music/Login.tsx
@@ -1,36 +1,41 @@
-import {View, Text} from "react-native";
+import {Text, TouchableOpacity, View} from "react-native";
 import * as AuthSession from 'expo-auth-session';
+import Constants from 'expo-constants'
+import Const from "ajv/lib/vocabularies/validation/const";
 
 export default function Login() {
-    async function authenticate() {
-        const discovery = {
-            authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-            tokenEndpoint: 'https://accounts.spotify.com/api/token',
-        }
-        const config : AuthSession.AuthRequestConfig = {
-            clientId: 'a6ece74c3c2646ae91ea8cdc34004aaf',
-            scopes: [
-                'user-read-private',
-                'user-read-email',
-                'user-library-read',
-                'playlist-read-private',
-                'user-read-recently-played',
-                'user-top-read',
-                'playlist-read-collaborative',
-                'playlist-modify-public',
-            ],
-            usePKCE: false,
-            redirectUri: AuthSession.makeRedirectUri({
-                scheme: 'myapp',
-            }),
-        }
-        const result = AuthSession.useAuthRequest(config, discovery);
-        console.log(result);
+    const discovery = {
+        authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+        tokenEndpoint: 'https://accounts.spotify.com/api/token',
     }
+    const config : AuthSession.AuthRequestConfig = {
+        responseType: AuthSession.ResponseType.Token,
+        clientId: Constants.manifest.extra.spotifyClientId,
+        clientSecret: process.env.CLIENT_SECRET,
+        scopes: [
+            'user-read-currently-playing',
+            'user-read-recently-played',
+            'user-read-playback-state',
+            'user-top-read',
+            'user-modify-playback-state',
+            'streaming',
+            'user-read-email',
+            'user-read-private'
+        ],
+        usePKCE: false,
+        redirectUri: AuthSession.makeRedirectUri({
+            scheme: 'myapp',
+        }),
+    }
+    const [request, response, promptAsync] = AuthSession.useAuthRequest(config, discovery);
+
+
 
     return (
-        <View className={'flex justify-center items-center h-full'}>
-            <Text className={'text-3xl'}>Music</Text>
+        <View className={'h-full flex justify-center items-center'}>
+            <TouchableOpacity className={'flex justify-center items-center border-4'} onPress={() => promptAsync()}>
+                <Text className={'text-3xl p-5'}>Login</Text>
+            </TouchableOpacity>
         </View>
     )
 }
