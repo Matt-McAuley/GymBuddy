3e199f09d3da2e19b99f54d02297eb2ec99b9cdb
Matthew McAuley <mattwilliam618@gmail.com>
2024-12-08 23:30:11 -0500

    Implement toy slide animation

--- components/Home/ExerciseDisplay.tsx
+++ components/Home/ExerciseDisplay.tsx
@@ -2,19 +2,31 @@ import {useStore} from "@/store";
 import PrimaryExercise from '@/components/Home/exerciseDisplays/PrimaryExercise';
 import AccessoryExercise from '@/components/Home/exerciseDisplays/AccessoryExercise';
 import SuperSet from '@/components/Home/exerciseDisplays/SuperSet';
+import {useEffect} from "react";
+import {Animated, Easing, useAnimatedValue} from "react-native";
 
 export default function ExerciseDisplay() {
     const {isAccessoryExercise, isPrimaryExercise, isSuperSet} = useStore();
     const exercise = useStore((state) => state.exercise());
 
-    return (isPrimaryExercise(exercise)) ? (
-        <PrimaryExercise />
+    const slideAnim = useAnimatedValue(1000);
+    useEffect(() => {
+        slideAnim.setValue(1000);
+        Animated.timing(slideAnim, {
+            toValue: 0,
+            duration: 500,
+            easing: Easing.ease,
+            useNativeDriver: true,
+        }).start();
+    }, [exercise]);
 
-    ) : (isAccessoryExercise(exercise)) ? (
-        <AccessoryExercise />
+    const exerciseComponent = (isPrimaryExercise(exercise)) ? <PrimaryExercise />
+        : (isAccessoryExercise(exercise)) ? <AccessoryExercise />
+            : (isSuperSet(exercise)) ? <SuperSet /> : null;
 
-    ) : (isSuperSet(exercise)) ? (
-        <SuperSet />
-
-    ) : null;
+    return (
+        <Animated.View style={{transform: [{ translateX: slideAnim }]}}>
+            {exerciseComponent}
+        </Animated.View>
+    )
 }
