d64a3946279029259caa21ba0246d553b2ec44c4
Matthew McAuley <mattwilliam618@gmail.com>
2024-12-27 18:17:11 -0500

    Get rid of unused store functions

--- store.tsx
+++ store.tsx
@@ -26,26 +26,6 @@ const useStore = create<storeType>((set) => ({
         set({ timesReset: useStore.getState().timesReset + 1 });
     },
 
-    nextExerciseHandler: () => {
-        const currentExercise = useStore.getState().currentExercise;
-        const nextExercise = useStore.getState().nextExercise();
-        const setCurrentExercise = useStore.getState().setCurrentExercise;
-
-        if (nextExercise === null) return;
-
-        setCurrentExercise(currentExercise + 1);
-    },
-
-    prevExerciseHandler: () => {
-        const currentExercise = useStore.getState().currentExercise;
-        const prevExercise = useStore.getState().prevExercise();
-        const setCurrentExercise = useStore.getState().setCurrentExercise;
-
-        if (prevExercise === null) return;
-
-        setCurrentExercise(currentExercise - 1);
-    },
-
     program: null,
     setProgram: (newProgram) => {set({ program: newProgram });},
 
@@ -63,12 +43,6 @@ const useStore = create<storeType>((set) => ({
 
     exercise: (): primaryExerciseType | accessoryExerciseType | superSetType =>
         useStore.getState().program!.days[useStore.getState().currentDay].exercises[useStore.getState().currentExercise],
-    prevExercise: ():  primaryExerciseType | accessoryExerciseType | superSetType | null => (useStore.getState().currentExercise > 0) ?
-        useStore.getState().program!.days[useStore.getState().currentDay].exercises[useStore.getState().currentExercise - 1]
-        : null,
-    nextExercise: (): primaryExerciseType | accessoryExerciseType | superSetType | null => (useStore.getState().currentExercise < useStore.getState().program!.days[useStore.getState().currentDay].exercises.length-1) ?
-        useStore.getState().program!.days[useStore.getState().currentDay].exercises[useStore.getState().currentExercise + 1]
-        : null,
 
     db: SQLite.openDatabaseSync('programs.db'),
 }));
@@ -112,16 +86,12 @@ type storeType = {
     timesReset: number,
     setTimesReset: (newTimesReset: number) => void,
     reset: () => void,
-    nextExerciseHandler: () => void,
-    prevExerciseHandler: () => void,
     program: programType | null,
     setProgram: (newProgram: programType | null) => void,
     isPrimaryExercise: (exercise: primaryExerciseType | accessoryExerciseType | superSetType) => exercise is primaryExerciseType,
     isAccessoryExercise: (exercise: primaryExerciseType | accessoryExerciseType | superSetType) => exercise is accessoryExerciseType,
     isSuperSet: (exercise: primaryExerciseType | accessoryExerciseType | superSetType) => exercise is superSetType,
     exercise: () => primaryExerciseType | accessoryExerciseType | superSetType,
-    prevExercise: () => primaryExerciseType | accessoryExerciseType | superSetType | null,
-    nextExercise: () => primaryExerciseType | accessoryExerciseType | superSetType | null,
     db: SQLite.SQLiteDatabase,
 }
 
