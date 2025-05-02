efe71470017addba15338dd98e40e52ff6483be6
Matt McAuley <mattwilliam618@gmail.com>
2025-05-02 17:23:09 -0400

    Re-Adding basic updates without breaking anything

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,7 @@ import {Image, Text, View} from "react-native";
 export default function Header() {
   return (
     <View className={'flex-row justify-center items-end h-36 bg-gray-500 pb-5'}>
-        <Image className={"h-16 w-16"} source={require("@/assets/images/LiftingLogo.png")}/>
         <Text className={"text-black-500 font-extrabold text-5xl"}>GymBuddy</Text>
-        <Image className={"h-16 w-16"} source={require("@/assets/images/LiftingLogo.png")}/>
     </View>
   );
 }
