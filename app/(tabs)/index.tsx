bac8ee8d9a8546ef00e2632e442b204acd71f129
Matt McAuley <mattwilliam618@gmail.com>
2025-05-03 13:40:38 -0400

    Add back basic changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import {View, Text, ScrollView, Dimensions, AppState} from "react-native";
+import {View, Text, ScrollView, Dimensions, AppState, StyleSheet} from "react-native";
 import Timer from "@/components/Home/Timer";
 import Counter from "@/components/Home/Counter";
 import ExerciseDisplay from "@/components/Home/ExerciseDisplay";
 import {useEffect, useRef, useState} from "react";
 import {addMockProgram, dbSetup, dbTeardown, getProgram} from "@/db/dbFunctions";
+import { Dropdown } from "react-native-element-dropdown";
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin";
 import {useStore} from "@/store";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -11,9 +12,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function Index() {
     const {db, program, setProgram, currentDay, setCurrentDay, setCurrentExercise, timesReset, paused,
         setCurrentScheme, setSet, setRetrievedTime, currentExercise, setRetrievedPaused, setRetrievedYet, setRetrievedSet, time, setTime} = useStore();
+    const day = program?.days[currentDay];
     const {width} = Dimensions.get('window');
-    const dayScrollRef = useRef<ScrollView | null>(null);
-    const exerciseScrollRefs = useRef<{ [key: number]: ScrollView | null }>({});
+    const exerciseScrollRef = useRef<ScrollView | null >(null);
     const [backgroundStart, setBackgroundStart] = useState<number | null>(null);
     const appState = useRef(AppState.currentState);
     useDrizzleStudio(db);
@@ -49,10 +50,7 @@ export default function Index() {
         const day = await AsyncStorage.getItem('currentDay');
         const exercise = await AsyncStorage.getItem('currentExercise');
         const scheme = await AsyncStorage.getItem('currentScheme');
-        dayScrollRef.current?.scrollTo({x: parseInt(day || '0') * width, y: 0, animated: false});
-        Object.keys(exerciseScrollRefs.current).forEach((key) => {
-            exerciseScrollRefs.current[parseInt(key)]?.scrollTo({x: parseInt(exercise || '0') * width, y: 0, animated: false});
-        });
+        exerciseScrollRef.current?.scrollTo({x: parseInt(exercise || '0') * width, y: 0, animated: false});
         setCurrentDay(parseInt(day || '0'));
         setCurrentExercise(parseInt(exercise || '0'));
         setCurrentScheme(scheme || '5 x 5');
@@ -70,17 +68,6 @@ export default function Index() {
         });
     }, []);
 
-    const handleDayScroll = (event: any) => {
-        const contentOffsetX = event.nativeEvent.contentOffset.x;
-        const day = Math.min(Math.max(Math.ceil((contentOffsetX-195) / width), 0), program!.days.length - 1);
-        if (day != currentDay) {
-            Object.keys(exerciseScrollRefs.current).forEach((key) => {
-                exerciseScrollRefs.current[parseInt(key)]?.scrollTo({x: 0, y: 0, animated: false});
-            });
-            setCurrentDay(day);
-        }
-    }
-
     const handleExerciseScroll = (event: any) => {
         const contentOffsetX = event.nativeEvent.contentOffset.x;
         const exercise = Math.min(Math.max(Math.ceil((contentOffsetX-195) / width), 0), program!.days[currentDay].exercises.length - 1);
@@ -90,51 +77,93 @@ export default function Index() {
     }
 
     useEffect(() => {
-        Object.keys(exerciseScrollRefs.current).forEach((key) => {
-            exerciseScrollRefs.current[parseInt(key)]?.scrollTo({x: 0, y: 0, animated: false});
-        });
-        dayScrollRef.current?.scrollTo({x: 0, y: 0, animated: false});
+        exerciseScrollRef.current?.scrollTo({x: 0, y: 0, animated: false});
     }, [timesReset]);
 
-  return (program == null) ? (
-      <View className={'flex justify-center items-center h-full'}>
-          <Text className={'text-3xl'}>No Program Selected</Text>
-      </View>
-      )
-      : (program.days.length == 0) ? (
+    return (program == null) ? (
             <View className={'flex justify-center items-center h-full'}>
-                <Text className={'text-3xl'}>No Days in Program</Text>
+                <Text className={'text-3xl'}>No Program Selected</Text>
             </View>
-        ) :
-      (
-          <ScrollView ref={dayScrollRef} snapToInterval={width} decelerationRate={'fast'} horizontal onScroll={handleDayScroll} pagingEnabled>
-              {program.days.map((day, dayIndex) => (
-                  <View key={dayIndex} style={{width}} className={'flex-1 flex-col justify-start items-center w-full gap-4 p-3'}>
-                      <View className={'h-20'}>
-                      <ScrollView horizontal contentContainerStyle={{alignItems: 'center', height: '100%'}}>
-                        <Text className={`text-center text-6xl font-bold`} style={{color: day.color}}>{day.name}</Text>
-                      </ScrollView>
-                      </View>
-                             {(day.exercises.length == 0) ?
-                                 <View className={'flex-col h-full justify-center items-center'}>
-                                     <Text className={'text-3xl'}>No Exercises in Day</Text>
-                                 </View>
-                                 :
-                                 <>
-                                     <Timer/>
-                                     <Counter/>
-                                     <ScrollView ref={(ref) => (exerciseScrollRefs.current[dayIndex] = ref)} horizontal snapToInterval={width} decelerationRate={'fast'} pagingEnabled onScroll={handleExerciseScroll}>
-                                         {day.exercises.map((exercise, exerciseIndex) => (
-                                             <View key={exerciseIndex} style={{width: width-20, marginRight: 20, padding: 3}}>
-                                                 <ExerciseDisplay currentExercise={exerciseIndex} currentDay={dayIndex}/>
-                                             </View>
-                                         ))}
-                                     </ScrollView>
-                                 </>
-                             }
-                  </View>
-                  ))}
-          </ScrollView>
-
-  );
+        )
+        : (program.days.length == 0) ? (
+                <View className={'flex justify-center items-center h-full'}>
+                    <Text className={'text-3xl'}>No Days in Program</Text>
+                </View>
+            ) :
+            (
+                <>
+                    <View className={'flex justify-center items-center'}>
+                        <Dropdown style={styles.dropdown} selectedTextStyle={{...styles.selected, color:day?.color}}
+                                  data={program.days.map((d, index) => ({label: d.name, value: {index, color: d.color}}))}
+                                  labelField='label' valueField='value' value={{label: day?.name, value: {index: currentDay, color: day?.color}}}
+                                  renderRightIcon={() => (
+                                      <Text style={{...styles.icon, color: 'black'}}>{'▼'}</Text>
+                                  )}
+                                  renderItem={(item, selected) => (
+                                      (selected) ? null :
+                                          <View style={styles.item}>
+                                              <Text style={{...styles.itemText, color: item.value.color}}>{item.label}</Text>
+                                          </View>)}
+                                  onChange={(item) => {setCurrentDay(item.value.index)}}/>
+                    </View>
+                    <View style={{width}} className={'flex-1 flex-col justify-start items-center w-full gap-4 p-3'}>
+                        {(day?.exercises.length == 0) ?
+                            <View className={'flex-col h-full justify-center items-center'}>
+                                <Text className={'text-3xl'}>No Exercises in Day</Text>
+                            </View>
+                            :
+                            <>
+                                <Timer/>
+                                <Counter/>
+                                <ScrollView ref={(ref) => (exerciseScrollRef.current = ref)} horizontal snapToInterval={width}
+                                            decelerationRate={'fast'} pagingEnabled onScroll={handleExerciseScroll}>
+                                    {day?.exercises.map((exercise, exerciseIndex) => (
+                                        <View key={exerciseIndex} style={{width: width-20, marginRight: 20, padding: 3}}>
+                                            <ExerciseDisplay currentExercise={exerciseIndex} currentDay={currentDay}/>
+                                        </View>
+                                    ))}
+                                </ScrollView>
+                            </>
+                        }
+                    </View>
+                </>
+            );
 }
+
+const styles = StyleSheet.create({
+    dropdown: {
+        height: 60,
+        width: '90%',
+        display: 'flex',
+        backgroundColor: 'white',
+        borderRadius: 8,
+        borderWidth: 1,
+        margin: 10,
+        padding: 10,
+
+    },
+    selected: {
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    icon: {
+        marginLeft: 5,
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    item: {
+        padding: 10,
+        height: 60,
+        borderBottomWidth: 2,
+        borderBottomColor: 'gray',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    itemText: {
+        fontSize: 30,
+        color: 'black',
+        fontWeight: 'bold',
+    },
+});
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,9 @@
-import {Image, Text, View} from "react-native";
+import {Text, View} from "react-native";
 
 export default function Header() {
   return (
     <View className={'flex-row justify-center items-end h-36 bg-gray-500 pb-5'}>
-        <Image className={"h-16 w-16"} source={require("@/assets/images/LiftingLogo.png")}/>
         <Text className={"text-black-500 font-extrabold text-5xl"}>GymBuddy</Text>
-        <Image className={"h-16 w-16"} source={require("@/assets/images/LiftingLogo.png")}/>
     </View>
   );
 }
