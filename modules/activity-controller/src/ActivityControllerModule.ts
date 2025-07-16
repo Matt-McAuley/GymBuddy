ba49459b558fae03655b2c3ac208fa076433608f
Matt McAuley <mattwilliam618@gmail.com>
2025-07-15 20:01:55 -0400

    Update module functions

--- modules/activity-controller/src/ActivityControllerModule.ts
+++ modules/activity-controller/src/ActivityControllerModule.ts
@@ -3,18 +3,16 @@ import * as types from './ActivityController.types';
 
 const nativeModule = requireNativeModule('ActivityController');
 
-export const startLiveActivity: types.startLiveActivityFn = async (timestamp) => {
-  const timestampInSeconds = timestamp / 1000;
-  return nativeModule.startLiveActivity(timestampInSeconds);
+export const startLiveActivity: types.startLiveActivityFn = async (time) => {
+  return nativeModule.startLiveActivity(time);
 }
 
 export const stopLiveActivity: types.stopLiveActivityFn = async () => {
   return nativeModule.stopLiveActivity();
 }
 
-export const pause: types.pauseFn = async (timestamp) => {
-  const timestampInSeconds = timestamp / 1000;
-  return nativeModule.pause(timestampInSeconds);
+export const pause: types.pauseFn = async () => {
+  return nativeModule.pause();
 }
 
 export const resume: types.resumeFn = async () => {
